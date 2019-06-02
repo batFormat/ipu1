d1c242f60c8f7e9f6d4a15e8973d8b386dc89e80
Oleg <ollegozz@yandex.ru>
2019-06-02 23:46:14 +0300

    Initial commit

---
+++ login.php
@@ -0,0 +1,45 @@
+<?php
+require 'db.php';
+
+$data = $_POST;
+if ( isset($data['do_login']) )
+{
+    $user = R::findOne('ls', 'nlic = ?', array($data['nlic'])); //ls таблица в бд
+    if ( $user )
+    {
+        //логин существует
+        if($data['house'] === $user->house) //проверка соостветсвия пароля
+        {
+            //если пароль совпадает, то нужно авторизовать пользователя
+            $_SESSION['logged_user'] = $user;
+            echo '<div style="color:green;">Ваш адрес <br/>Передать показания <a href="/">главную</a> страницу.</div><hr>';
+        }else
+        {
+            $errors[] = 'Неверно введен пароль!';
+        }
+
+    }else
+    {
+        $errors[] = 'Пользователь с таким логином не найден!';
+    }
+
+    if ( ! empty($errors) )
+    {
+        //выводим ошибки авторизации
+        echo '<div idls="errors" style="color:red;">' .array_shift($errors). '</div><hr>';
+    }
+
+}
+
+?>
+
+
+<form action="login.php" method="POST">
+    <strong>Логин</strong>
+    <input type="text" name="nlic" value="<?php echo @$data['nlic']; ?>"><br/>
+
+    <strong>Пароль</strong>
+    <input type="varchar" name="house" value="<?php echo @$data['house']; ?>"><br/>
+
+    <button type="submit" name="do_login">Войти</button>
+</form>
